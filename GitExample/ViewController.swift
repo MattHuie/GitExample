53b89759d5ec76d33ee097f8d2e4e6e7072330e1
MattHuie <matthuie93@gmail.com>
2019-02-08 10:54:55 -0500

    first commit

---
+++ GitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitExample
+//
+//  Created by Matthew Huie on 2/8/19.
+//  Copyright © 2019 Matthew Huie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("working on git")
+    }
+
+
+}
+
